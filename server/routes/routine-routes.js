6b6dd0e12804e4807dddd9da5efca69b895d42a9
Simon Lundh <simon.lundh@live.se>
2016-03-03 20:26:34 +0100

    asd

--- server/routes/routine-routes.js
+++ server/routes/routine-routes.js
@@ -28,12 +28,12 @@ module.exports = (function () {
                     task.save(function (err) {
                         if (err)
                             console.log(err)
-                    res.json(task[0])
+                    res.json(task[0].todos)
                     });
                 })
             }
             else{
-                res.json(task[0]);
+                res.json(task[0].todos);
             }
 
         })
